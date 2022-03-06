0a32dad79f3a46a7a923e9eeba64e40d6c2ebdd6
maxgfr <25312957+maxgfr@users.noreply.github.com>
2022-03-06 18:57:01 +0100

    fix: jest config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,14 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   transform: {
-    '^.+\\.(t|j)sx?$': ['@swc/jest'],
+    '^.+\\.(t|j)s$': [
+      '@swc/jest',
+      {
+        jsc: {
+          target: 'es2021',
+        },
+      },
+    ],
   },
   setupFiles: ['dotenv/config'],
   testPathIgnorePatterns: ['/node_modules/', '/build/'],
