e8a8cade40d0c1bd594d0ca72c0c9fc30df99c24
maxgfr <25312957+maxgfr@users.noreply.github.com>
2023-12-19 17:36:16 +0100

    ci

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,21 @@
-module.exports ={
-  "env": {
-    "browser": false,
-    "es6": true,
-    "node": true,
-    "jest/globals": true
+module.exports = {
+  env: {
+    browser: false,
+    es6: true,
+    node: true,
+    'jest/globals': true,
   },
-  "parser": "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: "./tsconfig.json",
+    project: './tsconfig.json',
     tsconfigRootDir: __dirname,
-},
-  "plugins": ["@typescript-eslint", "jest", "prettier"],
-  "extends": [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    "prettier"
+  },
+  plugins: ['@typescript-eslint', 'jest', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
   ],
-  "rules": {},
-  "ignorePatterns": ["**/*.cjs", "**/*.mjs", "**/*.js"],
-}
+  rules: {},
+  ignorePatterns: ['**/*.cjs', '**/*.mjs', '**/*.js'],
+};
